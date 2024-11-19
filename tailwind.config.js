3fc677496f524342292f17fbbc94e00719784244
Tinz2 <tinapat2547@gmail.com>
2024-11-19 20:11:50 +0700

    final2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans:['Kanit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
